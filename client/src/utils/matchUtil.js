26662fad33b8a03dab750fec4add2a0c78fd421c
Ohinoyi Moiza <163584196+Ohimoiza1205@users.noreply.github.com>
2025-09-25 01:37:39 -0500

    generate codes

--- client/src/utils/matchUtil.js
+++ client/src/utils/matchUtil.js
@@ -1,5 +1,5 @@
 /**
- * @param {number} length Length of code we want to generate
+ * @param {number} length Length of code(s) we want to generate
  * @returns A random series of letters and numbers of the specified length
  */
 function createGameCode(length) {
